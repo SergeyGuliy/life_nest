7f7bf04225011bc2c19ce3cea6786b7384532412
sergey <SergeyGuliy>
2021-05-10 00:56:38 +0300

    Cleaning Code

--- src/modules/chats/chats.gateway.ts
+++ src/modules/chats/chats.gateway.ts
@@ -1,42 +1,17 @@
 import {
   SubscribeMessage,
   WebSocketGateway,
-  OnGatewayInit,
   WebSocketServer,
-  OnGatewayConnection,
-  OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
 import { ChatsService } from './chats.service';
 import { MessageReceiverTypes } from '../../plugins/database/entities/enums';
 
-const mapOfUsers = {};
-
 @WebSocketGateway()
-export class ChatsGateway
-  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+export class ChatsGateway {
   constructor(private chatService: ChatsService) {}
 
   @WebSocketServer() server: Server;
-
-  @SubscribeMessage('msgToServer')
-  public handleMessage(client: Socket, payload: any) {
-    // console.log(payload);
-    // client.emit('joinedRoom', payload);
-  }
-
-  @SubscribeMessage('joinRoom')
-  public joinRoom(client: Socket, room: string): void {
-    // console.log(room);
-    // client.join(room);
-    // client.emit('joinedRoom', room);
-  }
-
-  @SubscribeMessage('leaveRoom')
-  public leaveRoom(client: Socket, room: string): void {
-    // client.leave(room);
-    // client.emit('leftRoom', room);
-  }
 
   @SubscribeMessage('messageToServer')
   public async messageToServer(client: Socket, messageToServer): Promise<void> {
@@ -64,10 +39,4 @@ export class ChatsGateway
   public sendMessageToClient(room, messageData): void {
     this.server.to(room).emit('messageToClient', messageData);
   }
-
-  public handleConnection(client: Socket): void {}
-
-  public handleDisconnect(client: Socket): void {}
-
-  public afterInit(client: Server): void {}
 }
--- src/modules/rooms/rooms.gateway.ts
+++ src/modules/rooms/rooms.gateway.ts
@@ -1,17 +1,13 @@
 import {
   SubscribeMessage,
   WebSocketGateway,
-  OnGatewayInit,
   WebSocketServer,
-  OnGatewayConnection,
-  OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
 import { MessageReceiverTypes } from '../../plugins/database/entities/enums';
 
 @WebSocketGateway()
-export class RoomsSocketGateway
-  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+export class RoomsSocketGateway {
   @WebSocketServer() server: Server;
 
   public roomInListCreated(roomData): void {
@@ -38,22 +34,14 @@ export class RoomsSocketGateway
   @SubscribeMessage('subscribeRoomsUpdate')
   public subscribeRoomsUpdate(client: Socket): void {
     client.join('RoomsUpdater');
-    // console.log(this.server.sockets.adapter.rooms.get('RoomsUpdater'));
   }
 
   @SubscribeMessage('unSubscribeRoomsUpdate')
   public unSubscribeRoomsUpdate(client: Socket): void {
     client.leave('RoomsUpdater');
-    // console.log(this.server.sockets.adapter.rooms.get('RoomsUpdater'));
   }
 
   public leaveRoom(userId): void {
     this.server.to(userId).emit('leaveRoom');
   }
-
-  public handleConnection(client: Socket): void {}
-
-  public handleDisconnect(client: Socket): void {}
-
-  public afterInit(client: Server): void {}
 }
