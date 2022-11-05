24c675c69d63cbbc26d4762dcf57e2e97ba2adf7
sergey <sergey.guliy.92@gmail.com>
2022-11-05 19:07:39 +0200

    rework math service to be module

--- src/assets/mathjs/index.ts
+++ src/assets/mathjs/index.ts
@@ -1,8 +1,23 @@
 import { create, all } from 'mathjs';
 
 import { customFunctions } from '@assets/mathjs/custom-functions';
+declare module 'mathjs' {
+  interface MathJsChain<TValue> {
+    percent(this: any, n?: any): any;
+  }
+}
+const $math = create(all);
+$math.import(customFunctions);
 
-const mathjs = create(all);
-mathjs.import(customFunctions);
+const ROUND = 2;
 
-export { mathjs };
+const $mMethods = {
+  $mRandom(min, max) {
+    return $math.round($math.random(min, max), ROUND);
+  },
+  $mChain(val) {
+    return $math.chain(val);
+  },
+};
+
+export { $math, $mMethods };
--- src/modules-helpers/global-services/global-services.module.ts
+++ src/modules-helpers/global-services/global-services.module.ts
@@ -3,20 +3,9 @@ import { Module } from '@nestjs/common';
 import { ErrorHandlerService } from './error-handler.service';
 import { MyLogger } from './my-logger.service';
 import { SocketNameSpacerService } from './socket-namespaser.service';
-import { MathService } from '@modules-helpers/global-services/math.service';
 
 @Module({
-  providers: [
-    ErrorHandlerService,
-    MyLogger,
-    SocketNameSpacerService,
-    MathService,
-  ],
-  exports: [
-    ErrorHandlerService,
-    MyLogger,
-    SocketNameSpacerService,
-    MathService,
-  ],
+  providers: [ErrorHandlerService, MyLogger, SocketNameSpacerService],
+  exports: [ErrorHandlerService, MyLogger, SocketNameSpacerService],
 })
 export class GlobalServicesModule {}
