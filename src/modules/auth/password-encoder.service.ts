511b902a401d97a697e622bc198169e3c6b02f84
sergey <sergey.guliy.92@gmail.com>
2023-02-04 17:00:23 +0300

    fix bcrypt

--- src/modules/auth/password-encoder.service.ts
+++ src/modules/auth/password-encoder.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-// import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt';
 
 import { SALT_ROUND_CRYPT } from 'life_shared/constants/index.js';
 
@@ -9,29 +9,27 @@ export class PasswordEncoderService {
 
   private checkPassword(password: string, hash: string): Promise<boolean> {
     return new Promise((resolve, reject) => {
-      // bcrypt.compare(password, hash, function (err, result) {
-      //   if (err) reject(err);
-      //   resolve(result);
-      // });
-      resolve(true);
+      bcrypt.compare(password, hash, function (err, result) {
+        if (err) reject(err);
+        resolve(result);
+      });
+      // resolve(true);
     });
   }
 
   public generatePasswordHash(password: string): Promise<string> {
     return new Promise((resolve, reject) => {
-      // bcrypt.genSalt(this.SALT_ROUND_CRYPT, function (err1, salt) {
-      //   bcrypt.hash(password, salt, function (err2, hash) {
-      //     if (err1) {
-      //       reject(err1);
-      //     } else if (err2) {
-      //       reject(err2);
-      //     } else {
-      //       resolve(hash);
-      //     }
-      //   });
-      // });
-
-      resolve(password);
+      bcrypt.genSalt(this.SALT_ROUND_CRYPT, function (err1, salt) {
+        bcrypt.hash(password, salt, function (err2, hash) {
+          if (err1) {
+            reject(err1);
+          } else if (err2) {
+            reject(err2);
+          } else {
+            resolve(hash);
+          }
+        });
+      });
     });
   }
 
